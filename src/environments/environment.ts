ffa822078a41a5b2e26b1803ad91616c1edc6fe9
ponsianodeloor <ponsianodeloor@gmail.com>
2024-07-30 21:40:45 -0500

    configuracion kc en ambiente de desarrollo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,7 @@
 export const environment = {
   baseUrl: 'http://localhost:3000',
+
+  KEYCLOAK_URL: 'https://sso-desa.funcionjudicial.gob.ec',
+  KEYCLOAK_REALM: 'notarial',
+  KEYCLOAK_CLIENT_ID: 'cj-web-app'
 }
